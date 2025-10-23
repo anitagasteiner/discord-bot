d5fed0289319aa094d6c0bb82110aa89e4a36e33
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-10-23 12:12:50 +0200

    create bot and prepare bot for start

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import discord
 from dotenv import load_dotenv
 import os
+from discord.ext import commands
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Ready! Ich bin {bot.user}.')
+
+bot.run(token)
+
+
 
