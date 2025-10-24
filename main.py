7f74b194d7c7d9c48d0a33315aedb74664689e7f
Anita Gasteiner <anita.gasteiner@posteo.de>
2025-10-24 10:16:16 +0200

    first bot reaction

--- main.py
+++ main.py
@@ -2,18 +2,35 @@ import discord
 from dotenv import load_dotenv
 import os
 from discord.ext import commands
+import logging
+
+handler = logging.FileHandler(filename='discordbot.log', mode='w', encoding='utf-8')
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'Ready! Ich bin {bot.user}.')
 
-bot.run(token)
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
 
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if 'banana' in message.content.lower():
+        await message.delete()
+        channel = message.channel
+        await channel.send(f'Hey, {message.author}! Dieser Begriff ist nur für die Schüler*innen der DA geeignet.')
+
+    await bot.process_commands(message)
 
 
